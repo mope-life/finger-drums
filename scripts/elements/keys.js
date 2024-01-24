b9c51c195ee0937c834154bd63dcb2f484c4317d
Dustin Ross <dross@raptor-scientific.com>
2024-01-23 20:34:45 -0500

    send entire key array on keys-control input
    Also perform some general code cleanup.

--- scripts/elements/keys.js
+++ scripts/elements/keys.js
@@ -1,102 +1,95 @@
-/* Converts MIDI note ids into frequencies. */
-/* Assumes twelve-tone equal temperament. Different temperaments may be implemented in future */
-function frequencyOf(noteId, a4 = 440) {
-  return a4 * 2**((noteId - 69) / 12);
-}
-
 class KeysControl extends HTMLElement {
   constructor() {
     super();
 
-    const templateContent = document.getElementById("keys-template").content;
+    const template = document.getElementById("keys-template");
     const shadowRoot = this.attachShadow({ mode: 'open' });
-    shadowRoot.appendChild(templateContent.cloneNode(true));
+    shadowRoot.appendChild(template.content.cloneNode(true));
 
     this._heldKeys = [];
     this._keyMap = (function () {
-      let pianoKeys = ['z', 's', 'x', 'd', 'c', 'v', 'g', 'b', 'h', 'n', 'j', 'm', ',', 'l', '.', ';', '/'];
+      let pianoKeys = [
+        'z', 's', 'x', 'd', 'c', 'v', 'g', 'b', 'h',
+        'n', 'j', 'm', ',', 'l', '.', ';', '/'
+      ];
       let map = new Map();
       pianoKeys.forEach((k, i) => {
-        map.set(k, i + 60); // MIDI key values
+        // Map keys to their MIDI codes, starting from middle C.
+        map.set(k, i + 60);
       });
       return map;
     })();
   }
 
+  // This implies that this element needs to either have a tabindex, or
+  // otherwise be fed keydown/keyup events.
   connectedCallback() {
-    document.addEventListener("keydown", this);
-    document.addEventListener("keyup", this);
+    this.addEventListener("keydown", this);
+    this.addEventListener("keyup", this);
   }
 
   disconnectedCallback() {
-    document.removeEventListener("keydown", this);
-    document.removeEventListener("keyup", this);
+    this.removeEventListener("keydown", this);
+    this.removeEventListener("keyup", this);
   }
 
   handleEvent(e) {
-    if (document.activeElement && document.activeElement.tagName === "INPUT") return;
-    if (e.type === "keydown") this._keyDownHandler(e);
-    else if (e.type === "keyup") this._keyUpHandler(e);
+    if (e.type === "keydown") {
+      this._keyDownHandler(e)
+    }
+    else if (e.type === "keyup") {
+      this._keyUpHandler(e);
+    }
   }
 
   _keyDownHandler(e) {
     const key = e.key.toLowerCase();
 
-    if (this._keyMap.has(key) && !this._heldKeys.includes(key)) {
+    if (this._keyMap.has(key)) {
       e.preventDefault();
       e.stopPropagation();
 
-      this._hold(key);
-      this._send(frequencyOf(this._keyMap.get(key)));
+      if (!this._heldKeys.includes(key)) {
+        this._heldKeys.unshift(key);
+
+        let keyDiv = this.shadowRoot.getElementById(key);
+        if (keyDiv) {
+          keyDiv.classList.add("held");
+        }
+
+        this._sendKeys();
+      }
     }
   }
 
   _keyUpHandler(e) {
     const key = e.key.toLowerCase();
 
-    if (this._keyMap.has(key) && this._heldKeys.includes(key)) {
+    if (this._keyMap.has(key)) {
       e.preventDefault();
       e.stopPropagation();
 
-      const lastPressed = this._release(key);
+      // since we will need the index anyway, use this instead of `includes`
+      const index = this._heldKeys.indexOf(key);
+      if (index >= 0) {
+        this._heldKeys.splice(index, 1);
 
-      if (this._heldKeys.length > 0) {
-        if (lastPressed) {
-          this._send(frequencyOf(this._keyMap.get(this._heldKeys[this._heldKeys.length - 1])));
+        let keyDiv = this.shadowRoot.getElementById(key);
+        if (keyDiv) {
+          keyDiv.classList.remove("held");
         }
-      } else {
-        this._send(0);
+
+        this._sendKeys();
       }
     }
   }
 
-  _hold(key) {
-    this._heldKeys.push(key);
-    let keyDiv = this.shadowRoot.getElementById(key);
-    if (keyDiv) {
-      keyDiv.classList.add("held");
-    }
-  }
-
-  // Returns whether the key we are releasing was also the last one pressed
-  _release(key) {
-    const idx = this._heldKeys.indexOf(key);
-    this._heldKeys.splice(idx, 1);
-
-    let keyDiv = this.shadowRoot.getElementById(key);
-    if (keyDiv) {
-      keyDiv.classList.remove("held");
-    }
-
-    return idx == this._heldKeys.length;
-  }
-
-  _send(freq) {
+  _sendKeys() {
     this.dispatchEvent(new CustomEvent(
       "input", {
       bubbles: true,
       cancelable: true,
-      detail: freq
+      detail: this._heldKeys
     }));
   }
 }
